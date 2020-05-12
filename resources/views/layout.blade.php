8841fe37dc919523e662a2c87c0e94ffdecee037
svlva <61283392+svlva@users.noreply.github.com>
2020-05-12 00:12:11 -0400

    Arreglado el problema que no dejaba dezplegar los menús al primer click, radicaba en plugins.js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,13 +1,9 @@
 @section('Head')
 <!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
 <head>
-    <title>@yield('title')</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ela Admin - HTML5 Admin Template</title>
     <meta name="description" content="Ela Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,45 +12,60 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
     <link rel="stylesheet" href=" {{asset('css/cs-skin-elastic.css')}}">
     <link rel="stylesheet" href=" {{asset('css/style.css')}}">
-    <link rel="stylesheet" href=" {{asset('css/inputmask.css')}}">
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
-    <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/brands.min.js" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/conflict-detection.min.js" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/fontawesome.min.js" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/regular.min.js" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/solid.min.js" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/v4-shims.min.js" rel="stylesheet" />
-    <link href=" {{asset('js/jquery.inputmask.bundle.js')}}" rel="stylesheet" />
-    <!--fullcalendar-->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <link href=" {{asset('js/calendar.js')}}" rel="stylesheet" />
-    <link href=" {{asset('js/fullcalendar.js')}}" rel="stylesheet" />
-    <link href=" {{asset('js/moment.min.js')}}" rel="stylesheet" />
-    <link href="https://code.jquery.com/jquery-1.11.3.min.js" rel="stylesheet" />
 
-    <!--END fullcalendar-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/brands.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/regular.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/solid.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/svg-with-js.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/v4-shims.min.css">
-    <!-- Fin Font Awesome -->
+    <!--librerías adicionales-->
+              <!-- Font Awesome -->
+              <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" rel="stylesheet" />
+              <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/brands.min.js" rel="stylesheet" />
+              <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/conflict-detection.min.js" rel="stylesheet" />
+              <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/fontawesome.min.js" rel="stylesheet" />
+              <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/regular.min.js" rel="stylesheet" />
+              <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/solid.min.js" rel="stylesheet" />
+              <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/v4-shims.min.js" rel="stylesheet" />
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/brands.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/regular.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/solid.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/svg-with-js.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/v4-shims.min.css">
+              <!--FIN:Font Awesome -->
+
+              <!--fullcalendar-->
+              <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+              <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+              <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+              <link href=" {{asset('js/calendar.js')}}" rel="stylesheet" />
+              <link href=" {{asset('js/fullcalendar.js')}}" rel="stylesheet" />
+              <link href=" {{asset('js/moment.min.js')}}" rel="stylesheet" />
+              <link href="https://code.jquery.com/jquery-1.11.3.min.js" rel="stylesheet" />
+              <!--FIN: fullcalendar-->
+
+              <!--InputMask-->
+              <link href=" {{asset('js/jquery.inputmask.bundle.js')}}" rel="stylesheet" />
+              <link rel="stylesheet" href=" {{asset('css/inputmask.css')}}">
+              <!--FIN:InputMask-->
+
+              <!--FLAG ICON-->
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+              <link rel="stylesheet" href=" {{asset('css/cs-skin-elastic.css')}}">
+              <!--FIN FLAG ICON-->
+
+    <!--FIN librerías adicionales-->
+
+
    <style>
     #weatherWidget .currentDesc {
         color: #ffffff!important;
@@ -95,7 +106,6 @@
 
 <body>
     <!-- Left Panel -->
-
     <style>
     #left-panel {
         overflow-y:auto;
@@ -147,8 +157,8 @@
             </div><!-- /.navbar-collapse -->
         </nav>
     </aside>
+    <!--FIN: left-panel -->
 
-    <!-- /#left-panel -->
     <!-- Right Panel -->
     <div id="right-panel" class="right-panel">
         <!-- Header-->
@@ -255,7 +265,6 @@
                 </div>
             </div>
         </header>
-
         <!-- /#header -->
 
         @yield('contenido')
@@ -279,17 +288,18 @@
     </div>
     <!-- /#right-panel -->
 
+    <!-- Ela Admin + Bootstrap Scripts -->
+
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
     <script src="{{asset('js/main.js')}}"></script>
-    <script src="{{asset('js/plugins.js')}}"></script>
 
     <!--  Chart js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.3/dist/Chart.bundle.min.js"></script>
-    <script src="{{asset('js/chartjs-init.js')}}"></script>
+
     <!--Chartist Chart-->
     <script src="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartist-plugin-legend@0.6.2/chartist-plugin-legend.min.js"></script>
@@ -305,9 +315,13 @@
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.js"></script>
     <script src="{{asset('calendar/fullcalendar-init.js')}}"></script>
 
+    <!-- Adicional-->
+    <script src="{{asset('js/chartjs-init.js')}}"></script>
+    <!-- FIN: Adicional-->
 
-
-    <!--FIN:Wickedpicker-->
+    <!--NO TOCAR-->
+    <!--<script src="{{asset('js/plugins.js')}}"></script>-->
+    <!--FIN: NO TOCAR-->
 
     <!--Local Stuff-->
     <script>
@@ -501,9 +515,28 @@
             // Bar Chart #flotBarChart End
         });
     </script>
-
+    <script type="text/javascript">
+      function soloNumeros(e){
+        key = e.keyCode || e.which;
+        tecla = String.fromCharCode(key).toLowerCase();
+        letras = "1234567890";
+        especiales = "8-37-39-46";
+        //--------------------------
+        tecla_especial = false
+        for(var i in especiales){
+          if(key == especiales[i]){
+            tecla_especial = true;
+            break;
+          }
+        }
+        //--------------------------
+        if(letras.indexOf(tecla)==-1 && !tecla_especial){
+          return false;
+        }
+      }
+    </script>
     <script>
-    function correoElectronico(e){
+      function correoElectronico(e){
       key = e.keyCode || e.which;
       tecla = String.fromCharCode(key).toLowerCase();
       letras = "1234567890abcdefghijklmnñopqrstuvwxyz-_~!$&'()*+,;=:@";
@@ -516,54 +549,33 @@
           break;
         }
       }
-    //--------------------------
-      if(letras.indexOf(tecla)==-1 && !tecla_especial){
-        return false;
-      }
-    }
-    </script>
-
-    <script>
-    function soloLetras(e){
-      key = e.keyCode || e.which;
-      tecla = String.fromCharCode(key).toLowerCase();
-      letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-      especiales = "8-37-39-46";
-
-      tecla_especial = false
-      for(var i in especiales){
-        if(key == especiales[i]){
-          tecla_especial = true;
-          break;
-        }
-      }
-
-      if(letras.indexOf(tecla)==-1 && !tecla_especial){
-        return false;
-      }
-    }
-    </script>
-
-    <script>
-    function soloNumeros(e){
-      key = e.keyCode || e.which;
-      tecla = String.fromCharCode(key).toLowerCase();
-      letras = "1234567890";
-      especiales = "8-37-39-46";
       //--------------------------
-      tecla_especial = false
-      for(var i in especiales){
-        if(key == especiales[i]){
-          tecla_especial = true;
-          break;
+        if(letras.indexOf(tecla)==-1 && !tecla_especial){
+          return false;
         }
       }
-    //--------------------------
-      if(letras.indexOf(tecla)==-1 && !tecla_especial){
-        return false;
-      }
-    }
     </script>
+    <script>
+      function soloLetras(e){
+        key = e.keyCode || e.which;
+        tecla = String.fromCharCode(key).toLowerCase();
+        letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+        especiales = "8-37-39-46";
+
+        tecla_especial = false
+        for(var i in especiales){
+          if(key == especiales[i]){
+            tecla_especial = true;
+            break;
+          }
+        }
+
+        if(letras.indexOf(tecla)==-1 && !tecla_especial){
+          return false;
+        }
+      }
+    </script>
+
 </body>
 </html>
 
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
 //usuarios
 Route::get('/', 'usuariosController@iniciarSesion');
 Route::get('/login', 'usuariosController@iniciarSesion');
